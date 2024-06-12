95c7c0d7b2b1d3ffd7166c79489bc7c1686ca96d
Govardhan <gkcanada007@gmail.com>
2024-06-12 12:02:12 -0600

    updated yaml and ts file configurations

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "@playwright/test";
 export default defineConfig({
-  reporter:[['junit', { outputFile: 'test-results/e2e-junit-results.xml' }]],
+  reporter:[['junit', { outputFile: 'test-results/e2e-junit-results.xml' }],
+            ['html', { outputFolder: 'playwright-report', open: 'never' }]],
 });
